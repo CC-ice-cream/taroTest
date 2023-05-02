9fff1d4eed22933b7df62f00d552b8fd3f4da13e
CC-ice-cream <45475641+CC-ice-cream@users.noreply.github.com>
2023-05-03 06:43:38 +0800

    merge stash3

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -7,7 +7,7 @@ import './index.scss'
 
 export default class Index extends Component<PropsWithChildren> {
   componentDidMount () { 
-    console.log("ferature")
+    console.log("ferature stash 33")
   }
 
   componentWillUnmount () { 
